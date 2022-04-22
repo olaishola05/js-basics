a4f1b336646ec107b8091957df3d946988c483de
Ola Ishola <olaishola@hotmail.co.uk>
2022-04-22 18:20:47 +0100

    Refactor the code to separate files

--- app.js
+++ app.js
@@ -1,28 +1,61 @@
-function makeCall(number){
-    return function(name) {
-        return `Call ${name} using this number: ${number}`
+import { Storage } from "./storage"
+
+const formBtn = document.querySelector('.formBtn')
+const name = document.getElementById('name')
+const age = document.getElementById('age')
+const form = document.querySelector('form')
+const storage = new Storage()
+const toast = document.querySelector('.toast')
+
+
+const toastMsg = (msg) => {
+    msg = 'New name added to the lists'
+    toast.textContent = msg
+    toast.classList.toggle('toast-success')
+    setTimeout(() => {
+        toast.classList.toggle('toast-success')
+    }, 3000);
+}
+
+const toastError = (err) => {
+    err = 'Input boxes can not be empty'
+    toast.textContent = err
+    toast.classList.toggle('toast-error')
+    setTimeout(() => {
+        toast.classList.toggle('toast-error')
+    }, 3000);
+}
+
+
+const submitForm = (e)=> {
+    e.preventDefault()
+    if(name.value === '' && age.value === ''){
+        toastError()
+        return null
+    }else{
+        const personInfo = {
+            name: name.value,
+            age: age.value
+        }
+        storage.setStorage(personInfo)
+        toastMsg()
+        form.reset()
     }
 }
 
-const ade = makeCall(09023456832)
-const jimi = makeCall(0123654869)
 
-// console.log(ade('Jones'))
-// console.log(jimi('Banlon'))
+const view = document.querySelector('.result')
+const displayContent = (data) => {
+    content = '';
+    data.forEach((person) =>{
+        content += `<div class="items">
+        <p>Name: <span id="nameResult">${person.name}</span></p>
+        <p>Age: <span id="ageResult">${person.age}</span></p>
+    </div>`
+    })
 
-function sendMessage(item){
-
-    function postMan(address){
-        return `Dispatch ${item} to the following address: ${address}`
-    }
-
-    return postMan
+    view.innerHTML = content
 }
 
-const mailer = sendMessage('Team Jerseys')
-const mailer_2 = sendMessage('Audi A16 Convertible')
-const mail = mailer('Lane 5, digital ocean view, ATL')
-const mail_2 = mailer_2('HebronHQ road 5, Ontario Canada')
-
-console.log(mail)
-console.log(mail_2)
+displayContent(storage.getItemFromStorage())
+formBtn.addEventListener('click', submitForm)
--- storage.js
+++ storage.js
@@ -1,4 +1,4 @@
-class Storage {
+export class Storage {
 
     constructor(){
         this.data = []
@@ -16,67 +16,3 @@ class Storage {
     }
 
 }
-
-const formBtn = document.querySelector('.formBtn')
-const name = document.getElementById('name')
-const age = document.getElementById('age')
-const form = document.querySelector('form')
-const storage = new Storage()
-const toast = document.querySelector('.toast')
-
-
-const toastMsg = (msg) => {
-    msg = 'New name added to the lists'
-    toast.textContent = msg
-    toast.classList.toggle('toast-success')
-    setTimeout(() => {
-        toast.classList.toggle('toast-success')
-    }, 3000);
-}
-
-const toastError = (err) => {
-    err = 'Input boxes can not be empty'
-    toast.textContent = err
-    toast.classList.toggle('toast-error')
-    setTimeout(() => {
-        toast.classList.toggle('toast-error')
-    }, 3000);
-}
-
-
-const submitForm = (e)=> {
-    e.preventDefault()
-    if(name.value === '' && age.value === ''){
-        toastError()
-        return null
-    }else{
-        const personInfo = {
-            name: name.value,
-            age: age.value
-        }
-        storage.setStorage(personInfo)
-        toastMsg()
-        form.reset()
-    }
-}
-
-
-formBtn.addEventListener('click', submitForm)
-
-
-const view = document.querySelector('.result')
-
-const displayContent = (data) => {
-    content = '';
-    data.forEach((person) =>{
-        content += `<div class="items">
-        <p>Name: <span id="nameResult">${person.name}</span></p>
-        <p>Age: <span id="ageResult">${person.age}</span></p>
-    </div>`
-    })
-
-    view.innerHTML = content
-}
-
-const data = storage.getItemFromStorage();
-displayContent(data)
