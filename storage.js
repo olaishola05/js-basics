8f2d448e8b6b8479ed8dfb78f16e6a329b5d20b9
Ola Ishola <olaishola@hotmail.co.uk>
2022-04-22 17:57:31 +0100

    Add form validation and error message

--- storage.js
+++ storage.js
@@ -22,27 +22,42 @@ const name = document.getElementById('name')
 const age = document.getElementById('age')
 const form = document.querySelector('form')
 const storage = new Storage()
+const toast = document.querySelector('.toast')
 
-const toastMsg = () => {
-    const toast = document.querySelector('.toast')
-    toast.style.visibility = 'visible'
-    toast.style.marginBottom = '10px'
+
+const toastMsg = (msg) => {
+    msg = 'New name added to the lists'
+    toast.textContent = msg
+    toast.classList.toggle('toast-success')
     setTimeout(() => {
-        toast.style.visibility = 'hidden'
-        toast.style.marginBottom = '0'
+        toast.classList.toggle('toast-success')
+    }, 3000);
+}
+
+const toastError = (err) => {
+    err = 'Input boxes can not be empty'
+    toast.textContent = err
+    toast.classList.toggle('toast-error')
+    setTimeout(() => {
+        toast.classList.toggle('toast-error')
     }, 3000);
 }
 
 
 const submitForm = (e)=> {
     e.preventDefault()
-    const personInfo = {
-        name: name.value,
-        age: age.value
+    if(name.value === '' && age.value === ''){
+        toastError()
+        return null
+    }else{
+        const personInfo = {
+            name: name.value,
+            age: age.value
+        }
+        storage.setStorage(personInfo)
+        toastMsg()
+        form.reset()
     }
-    storage.setStorage(personInfo)
-    toastMsg()
-    form.reset()
 }
 
 
